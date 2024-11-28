2ff3b4c3b2d60988476faaa4d2014a07d32e3237
David Dragovacz <david.dragovacz@remote.com>
2024-11-28 00:45:17 +0000

    feat(layout): clean up layout

--- apps/web/src/components/images.tsx
+++ apps/web/src/components/images.tsx
@@ -1,42 +1,27 @@
 import Image from "next/image";
 import { headers } from "next/headers";
-import BackgroundWrapper from "@/components/background-wrapper";
-import ImageRefresh from "@/components/image-refresh";
-import ClientColors from "@/components/client-colors";
-import ServerColors from "@/components/server-colors";
 
-export default async function Images() {
+import { cn } from "@/lib/cn";
+
+export async function Images() {
   const headersList = await headers();
   const timestamp = headersList.get("x-timestamp") || Date.now();
 
   const imageId = Math.floor(Math.random() * 1000);
-  const imageUrl = `https://picsum.photos/seed/${imageId}/400/400?t=${timestamp}`;
+  const imageUrl = `https://picsum.photos/seed/${imageId}/840/840?t=${timestamp}`;
+
+  console.log(imageUrl);
 
   return (
-    <>
-      <BackgroundWrapper imageUrl={imageUrl} />
-      <div className="flex flex-col items-center justify-center gap-6">
-        <div className="flex flex-col gap-2">
-          <p>Client</p>
-          <ClientColors imageUrl={imageUrl} />
-
-          <p>Server</p>
-          <ServerColors imageUrl={imageUrl} />
-        </div>
-
-        <div className="relative h-96 w-96 overflow-hidden rounded-2xl shadow-lg">
-          <Image
-            src={imageUrl}
-            alt="Random image from Picsum"
-            fill
-            sizes="400px"
-            className="object-cover"
-            priority
-          />
-        </div>
-
-        <ImageRefresh />
-      </div>
-    </>
+    <div className={cn("relative size-full")}>
+      <Image
+        src={imageUrl}
+        alt="Random image from Picsum"
+        fill
+        sizes="840px"
+        className="object-cover"
+        priority
+      />
+    </div>
   );
 }
