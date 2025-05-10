4c3682c028fa30808b0738c642d125ed2fa01299
David Dragovacz <david.dragovacz@remote.com>
2025-05-10 20:25:04 +0100

    feat(web): add util to get image paths randomly

---
+++ apps/web/src/lib/image.ts
@@ -0,0 +1,19 @@
+export function getImages(count: number, max: number): string[] {
+  if (count > max) {
+    throw new Error(
+      `[@drgd/aura] - Requested count exceeds total available images.`,
+    );
+  }
+
+  if (count <= 0) {
+    return [];
+  }
+
+  const nums = new Set<number>();
+
+  while (nums.size < count) {
+    nums.add(Math.floor(Math.random() * max) + 1);
+  }
+
+  return Array.from(nums, (num) => `/assets/${num}.webp`);
+}
